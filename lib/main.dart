cad31d50700e5b439f5f2dce26f5b0553a13675e
jonghyun <jonghyun@jonghyunui-MacBookPro.local>
2021-11-15 17:45:09 +0900

    MoolZal created

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+// 21-2 MAD Project (MoolZal)
+// 21700339 Jonghyun Baek
+// 21700557 Eunji Lee
+// 21900318 Chanyoung Park
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MoolZal',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MoolZal'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'MoolZal',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
