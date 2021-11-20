1aba3d26ab7fbc8cb91870a774762d2f5b3da8d2
jonghyun <jonghyun@jonghyucBookPro.T-mobile.com>
2021-11-20 16:16:56 +0900

    main.dart file modified

--- lib/main.dart
+++ lib/main.dart
@@ -4,48 +4,8 @@
 // 21900318 Chanyoung Park
 
 import 'package:flutter/material.dart';
+import 'package:moolzal/app.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MoolZal',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MoolZal'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'MoolZal',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(MoolZalApp());
 }
