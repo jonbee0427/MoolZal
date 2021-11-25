2229a756ce0c9eb764df04d8efe321521bfd5c3e
jonghyun <jonghyun@jonghyunui-MacBookPro.local>
2021-11-25 17:45:58 +0900

    JH/1125/category buttons modified

--- lib/category.dart
+++ lib/category.dart
@@ -19,9 +19,20 @@ class _CategoryState extends State<Category> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(
-                'Welcome!',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome!',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 30.0),
+                    child: IconButton(onPressed: (){print("Search Button");}, icon: Icon(Icons.search, size: 40,) ),
+                  ),
+
+                ],
               ),
               SizedBox(height: 10),
               Text(name, style: TextStyle(fontSize: 20),),
@@ -30,6 +41,7 @@ class _CategoryState extends State<Category> {
                 'Category',
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
+              // Row tile 1
               SizedBox(height: 20),
               Padding(
                 padding: const EdgeInsets.only(left: 20.0, right: 10.0),
@@ -46,17 +58,17 @@ class _CategoryState extends State<Category> {
                               color: Colors.lightBlue, // button color
                               child: InkWell(// splash color
                                 onTap: () {
-                                  print("영어 button");
+                                  print("국제어문 button");
                                 }, // button pressed
                                 child: Center(
                                   child:
-                                    Icon(Icons.language), // icon// text
+                                  Image.asset('lang.png', width: 40, height: 40), // icon// text
                                 ),
                               ),
                             ),
                           ),
                         ),
-                        Text("영어"),
+                        Text("국제어문", style: TextStyle(fontWeight: FontWeight.bold),),
                       ],
                     ),
                     SizedBox(width: 25,),
@@ -69,17 +81,17 @@ class _CategoryState extends State<Category> {
                               color: Colors.lightBlue, // button color
                               child: InkWell(// splash color
                                 onTap: () {
-                                  print("영어 button");
+                                  print("경영경제 button");
                                 }, // button pressed
                                 child: Center(
                                   child:
-                                  Icon(Icons.language), // icon// text
+                                  Image.asset('economics.png', width: 40, height: 40), // icon// text
                                 ),
                               ),
                             ),
                           ),
                         ),
-                        Text("영어"),
+                        Text("경영경제", style: TextStyle(fontWeight: FontWeight.bold),),
                       ],
                     ),
                     SizedBox(width: 25,),
@@ -92,17 +104,17 @@ class _CategoryState extends State<Category> {
                               color: Colors.lightBlue, // button color
                               child: InkWell(// splash color
                                 onTap: () {
-                                  print("영어 button");
+                                  print("법 button");
                                 }, // button pressed
                                 child: Center(
                                   child:
-                                  Icon(Icons.language), // icon// text
+                                  Image.asset('law.png', width: 40, height: 40), // icon// text
                                 ),
                               ),
                             ),
                           ),
                         ),
-                        Text("영어"),
+                        Text("법", style: TextStyle(fontWeight: FontWeight.bold),),
                       ],
                     ),
                     SizedBox(width: 25,),
@@ -115,17 +127,223 @@ class _CategoryState extends State<Category> {
                               color: Colors.lightBlue, // button color
                               child: InkWell(// splash color
                                 onTap: () {
-                                  print("영어 button");
+                                  print("공연영상 button");
                                 }, // button pressed
                                 child: Center(
                                   child:
-                                  Icon(Icons.language), // icon// text
+                                  Image.asset('film.png', width: 40, height: 40), // icon// text
                                 ),
                               ),
                             ),
                           ),
                         ),
-                        Text("영어"),
+                        Text("공연영상", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                  ],
+                ),
+              ),
+              // Row tile 2
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("공간환경 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('construct.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("공간환경", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("기계제어 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('mechanical.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("기계제어", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("디자인 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('design.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("디자인", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("생명과학 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('biology.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("생명과학", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                  ],
+                ),
+              ),
+              // Row tile 3
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("전산전자 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('csee.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("전산전자", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("상담심리 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('counsel.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("상담심리", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("ICT창업 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('enterpreneur.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("ICT창업", style: TextStyle(fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    SizedBox(width: 25,),
+                    Column(
+                      children: [
+                        SizedBox.fromSize(
+                          size: Size(60, 60), // button width and height
+                          child: ClipOval(
+                            child: Material(
+                              color: Colors.lightBlue, // button color
+                              child: InkWell(// splash color
+                                onTap: () {
+                                  print("기타 button");
+                                }, // button pressed
+                                child: Center(
+                                  child:
+                                  Image.asset('etc.png', width: 40, height: 40), // icon// text
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Text("기타", style: TextStyle(fontWeight: FontWeight.bold),),
                       ],
                     ),
                     SizedBox(width: 25,),
