e0aeb18b3b73cb6a6108cb10507e296329fde477
jonghyun <jonghyun@jonghyunui-MacBookPro.local>
2021-12-11 19:14:00 +0900

    JH/1211/circularprogress modified

--- lib/chart.dart
+++ lib/chart.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-
 class chart extends StatefulWidget {
   // ignore: prefer_const_constructors_in_immutables
   chart({Key? key}) : super(key: key);
@@ -12,26 +10,18 @@ class chart extends StatefulWidget {
   _chartState createState() => _chartState();
 }
 
-
-
 class _chartState extends State<chart> {
   late List<_ChartData> data;
   late TooltipBehavior _tooltip;
 
-  String ? firtName;
+  String? firtName;
   var secondName;
   var thirdName;
-  int ? check = 0;
-
-
+  int? check = 0;
 
   @override
   void initState() {
-
-    var list = FirebaseFirestore.instance
-        .collection('users')
-        .snapshots();
-
+    var list = FirebaseFirestore.instance.collection('users').snapshots();
 
     data = [
       _ChartData('CHN', 12),
@@ -44,119 +34,116 @@ class _chartState extends State<chart> {
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-          appBar: AppBar(
-            title: const Text('질문왕 랭킹'),
-            backgroundColor: Colors.deepPurple,
-          ),
-          body: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection('users')
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return new Text(
-                      'Error in receiving trip photos: ${snapshot.error}');
-                }
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('질문왕 랭킹'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection('users').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return new Text(
+                    'Error in receiving trip photos: ${snapshot.error}');
+              }
 
-                var PostsCount = 0;
-                List<DocumentSnapshot> itemImages;
+              var PostsCount = 0;
+              List<DocumentSnapshot> itemImages;
 
-                if (snapshot.hasData) {
-                  itemImages = snapshot.data!.docs;
-                  PostsCount = itemImages.length;
-                }
+              if (snapshot.hasData) {
+                itemImages = snapshot.data!.docs;
+                PostsCount = itemImages.length;
+              }
 
-                if(check == 0){
-                  check = 1;
-                  return CircularProgressIndicator();
-                }
+              if (check == 0) {
+                check = 1;
+                return Center(child: CircularProgressIndicator());
+              }
 
-                int len = snapshot.data!.docs.length;
-                List<String> names = ["NS"];
-                for (int i = 0; i < len; i++) {
-                  names.add(snapshot.data!.docs[i]['name']);
-                }
-                names.remove("NS");
-                List<int> count = [-1];
-                for (int i = 0; i < len; i++) {
-                  count.add(snapshot.data!.docs[i]['posts'].length);
-                }
-                count.remove(-1);
-                int temp = 0;
-                String Stemp = 'temp';
-                for (int i = 0; i < len; i++) {
-                  for (int j = 0; j < len - 1; j++) {
-                    if (count[j] > count[j + 1]) {
-                      temp = count[j + 1];
-                      count[j + 1] = count[j];
-                      count[j] = temp;
+              int len = snapshot.data!.docs.length;
+              List<String> names = ["NS"];
+              for (int i = 0; i < len; i++) {
+                names.add(snapshot.data!.docs[i]['name']);
+              }
+              names.remove("NS");
+              List<int> count = [-1];
+              for (int i = 0; i < len; i++) {
+                count.add(snapshot.data!.docs[i]['posts'].length);
+              }
+              count.remove(-1);
+              int temp = 0;
+              String Stemp = 'temp';
+              for (int i = 0; i < len; i++) {
+                for (int j = 0; j < len - 1; j++) {
+                  if (count[j] > count[j + 1]) {
+                    temp = count[j + 1];
+                    count[j + 1] = count[j];
+                    count[j] = temp;
 
-                      Stemp = names[j + 1];
-                      names[j + 1] = names[j];
-                      names[j] = Stemp;
-                    }
+                    Stemp = names[j + 1];
+                    names[j + 1] = names[j];
+                    names[j] = Stemp;
                   }
                 }
-                data = [
-                  _ChartData(names[len - 3], count[len - 3].toDouble()),
-                  _ChartData(names[len - 2], count[len - 2].toDouble()),
-                  _ChartData(names[len - 1], count[len - 1].toDouble()),
-                ];
-                check = 12;
-
-                if(check == 12) {
-                  return Column(
-                    children: <Widget>[
-                      SfCartesianChart(
-                          primaryXAxis: CategoryAxis(),
-                          primaryYAxis: NumericAxis(
-                              minimum: 0, maximum: 40, interval: 10),
-                          tooltipBehavior: _tooltip,
-                          series: <ChartSeries<_ChartData, String>>[
-                            ColumnSeries<_ChartData, String>(
-                                dataSource: data,
-                                xValueMapper: (_ChartData data, _) => data.x,
-                                yValueMapper: (_ChartData data, _) => data.y,
-                                name: 'Questions',
-                                color: Colors.deepPurple)
-                          ]
-                      ),
-                      Container(
-                        child: Text("***질문왕***", style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20)),
-                      ),
-                      Container(
-                        child: Text('1등 ' + names[len - 1], style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20)),
-                      ),
-                      Container(
-                        child: Text('2등 ' + names[len - 2], style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20)),
-                      ),
-                      Container(
-                        child: Text('3등 ' + names[len - 3], style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20)),
-                      ),
-                    ],
-                  );
-                }else {
-                  return CircularProgressIndicator();
-                }
               }
-          )
-      );
-    }
+              data = [
+                _ChartData(names[len - 3], count[len - 3].toDouble()),
+                _ChartData(names[len - 2], count[len - 2].toDouble()),
+                _ChartData(names[len - 1], count[len - 1].toDouble()),
+              ];
+              check = 12;
 
-
+              if (check == 12) {
+                return Column(
+                  children: <Widget>[
+                    SfCartesianChart(
+                        primaryXAxis: CategoryAxis(),
+                        primaryYAxis:
+                            NumericAxis(minimum: 0, maximum: 40, interval: 10),
+                        tooltipBehavior: _tooltip,
+                        series: <ChartSeries<_ChartData, String>>[
+                          ColumnSeries<_ChartData, String>(
+                              dataSource: data,
+                              xValueMapper: (_ChartData data, _) => data.x,
+                              yValueMapper: (_ChartData data, _) => data.y,
+                              name: 'Questions',
+                              color: Colors.deepPurple)
+                        ]),
+                    Container(
+                      child: Text("***질문왕***",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)),
+                    ),
+                    Container(
+                      child: Text('1등 ' + names[len - 1],
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)),
+                    ),
+                    Container(
+                      child: Text('2등 ' + names[len - 2],
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)),
+                    ),
+                    Container(
+                      child: Text('3등 ' + names[len - 3],
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)),
+                    ),
+                  ],
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
+  }
 }
 
 class _ChartData {
